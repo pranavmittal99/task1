c96b4a775f84ada1960ed9a3d0b871aaba8916f1
pranav mittal <pranav@bsgindia.com>
2016-09-11 13:37:37 +0530

    my fourth commit

--- index.php
+++ index.php
@@ -4,7 +4,12 @@
 	<title>my first task</title>
 </head>
 <body>
+	<h1>my first web page</h1>
+	<h6>my first line of web page</h6>
+	<h3>im awsome</h3>
 	<p>hello world</p>
 	<p>how you doing</p>
+	<h2>Spectacular Mountain</h2>
+<img src="images/pic_mountain.jpg" alt="Mountain View" style="width:1080px;height:1080px;">
 </body>
 </html>
